5354a15b14e3321539b48b059e7284640f170c1d
salari_adrian <adriansalari8@gmail.com>
2024-11-26 23:14:24 +0200

    add methods get, sleep, close, quit, switch, getWindowHandle, window, newWindow

--- src/main/java/org/example/TestRunner.java
+++ src/main/java/org/example/TestRunner.java
@@ -2,13 +2,23 @@ package org.example;
 
 import org.example.managers.DriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
 
 public class TestRunner {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = DriverManager.getInstance().getDriver();
-        driver.get("https://www.google.com/");
 
-        Thread.sleep(5000);
-        DriverManager.getInstance().getDriver().quit();
+        String  theNameOfTheFirstTab = driver.getWindowHandle();
+        Thread.sleep(3000);
+
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://999.md/");
+        Thread.sleep(3000);
+        driver.close();
+
+        driver.switchTo().window(theNameOfTheFirstTab);
+        driver.get("https://www.friv.com/");
+        Thread.sleep(3000);
+        driver.quit();
     }
 }
